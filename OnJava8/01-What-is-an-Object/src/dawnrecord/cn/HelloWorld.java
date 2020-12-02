c919456e3e03c99c9185e5a795439807aab27550
李晓旭 <li.xiaoxu@hotmail.com>
2020-12-02 11:51:15 +0800

    init

---
+++ OnJava8/01-What-is-an-Object/src/dawnrecord/cn/HelloWorld.java
@@ -0,0 +1,12 @@
+package dawnrecord.cn;
+
+/**
+ * @program: OnJava8
+ * @author: Dawn
+ * @create: 2020-12-02 11:49
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
